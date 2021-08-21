b618d01a4f7b198fb8062fc577050d5b1339c7d6
v.glushko <v.glushko@qsoft.ru>
2021-08-21 11:45:11 +0300

    Изменил класс настроек, добавил разные режимы

--- lib/settings.dart
+++ lib/settings.dart
@@ -9,20 +9,64 @@ class AppSettings {
 
   AppSettings._();
 
-  double _primaryFontSize = 24;
-  double _buttonFontSize = 24;
-  double _resultScreenFontSize = 32;
-  double _buttonBorderRadius = 20;
+  AppMode _currentMode = AppModes.STANDARD;
 
-  Color _numberButtonTextColor = Colors.white;
-  Color _operationButtonIconColor = Colors.orange;
+  AppMode get currentMode => _currentMode;
+
+  void changeMode(AppMode mode) {
+    _currentMode = mode;
+  }
+}
+
+abstract class AppMode {
+  const AppMode();
+
+  abstract final double _primaryFontSize;
+  abstract final double _buttonFontSize;
+  abstract final double _resultScreenFontSize;
+  abstract final double _buttonBorderRadius;
+
+  abstract final Color _buttonColor;
+  abstract final Color _numberButtonTextColor;
+  abstract final Color _operationButtonIconColor;
 
   double get primaryFontSize => _primaryFontSize;
   double get buttonFontSize => _buttonFontSize;
   double get resultScreenFontSize => _resultScreenFontSize;
   double get buttonBorderRadius => _buttonBorderRadius;
 
+  Color get buttonColor => _buttonColor;
   Color get numberButtonTextColor => _numberButtonTextColor;
   Color get operationButtonIconColor => _operationButtonIconColor;
+}
 
+class StandardMode extends AppMode {
+  const StandardMode();
+
+  final double _primaryFontSize = 24;
+  final double _buttonFontSize = 24;
+  final double _resultScreenFontSize = 32;
+  final double _buttonBorderRadius = 20;
+
+  final Color _buttonColor = Colors.grey;
+  final Color _numberButtonTextColor = Colors.black;
+  final Color _operationButtonIconColor = Colors.orange;
+}
+
+class ExtendedMode extends AppMode {
+  const ExtendedMode();
+
+  final double _primaryFontSize = 36;
+  final double _buttonFontSize = 36;
+  final double _resultScreenFontSize = 48;
+  final double _buttonBorderRadius = 30;
+
+  final Color _buttonColor = Colors.pinkAccent;
+  final Color _numberButtonTextColor = Colors.white;
+  final Color _operationButtonIconColor = Colors.green;
+}
+
+class AppModes {
+  static const AppMode STANDARD = const StandardMode();
+  static const AppMode EXTENDED = const ExtendedMode();
 }
